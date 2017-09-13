e12546f5ea076c51b42868924f37b429b5fca0de
pastuh <vpastuh@gmail.com>
2017-09-13 21:57:18 +0300

    Pataisytas failu konfigas

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => 'local',
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+    'cloud' => 's3',
 
     /*
     |--------------------------------------------------------------------------
@@ -61,6 +61,11 @@ return [
             'secret' => env('AWS_SECRET'),
             'region' => env('AWS_REGION'),
             'bucket' => env('AWS_BUCKET'),
+        ],
+
+        'images' => [
+            'driver' => 'local',
+            'root' => public_path('img/'),
         ],
 
     ],
