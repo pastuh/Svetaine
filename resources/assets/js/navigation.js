2fd7a3bcb33202b76e9f3a1b2424e27c1f62ebee
pastuh <vpastuh@gmail.com>
2017-09-22 22:09:21 +0300

    Scrollbar 13

--- resources/assets/js/navigation.js
+++ resources/assets/js/navigation.js
@@ -30,6 +30,43 @@ $(document).ready(function () {
         $("#main-form").submit();
     });
 
+    /* Show hide header */
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
 
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('header').removeClass('nav-down',250).addClass('nav-up', 250);
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up',250).addClass('nav-down', 250);
+            }
+        }
+
+        lastScrollTop = st;
+    }
 
 });
