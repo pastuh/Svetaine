7b2e96ffa2cc0a8aa5ed2f707c3e1241070f6d6d
pastuh <vpastuh@gmail.com>
2017-10-18 23:44:10 +0300

    GA fix2

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,22 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108229868-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-108229868-1');
-    </script>
-
     @include('partials._head')
 </head>
 
 {{-- Galimybe priskirti stiliu kiekvienam puslapiui atskirai --}}
 <body class="@yield('body_class')" style="@yield('body_style')">
-
+@include('partials._analyticstracking')
 
 <div class="preloader_active">
     <div class="main_preloader_load_back">
--- resources/views/layouts/manage.blade.php
+++ resources/views/layouts/manage.blade.php
@@ -1,21 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108229868-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-108229868-1');
-    </script>
-
     @include('partials._head')
 </head>
 
 {{-- Galimybe priskirti stiliu kiekvienam puslapiui atskirai --}}
 <body class="@yield('body_class')" style="@yield('body_style')">
+@include('partials._analyticstracking')
 
 <!-- Loading screen'as
 <div class="preloader_active">
